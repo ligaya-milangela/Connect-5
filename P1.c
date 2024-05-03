bc37f73ea25ddeb82d21f5fd14d3fc20baf6be97
ligaya-milangela <125577516+ligaya-milangela@users.noreply.github.com>
2024-05-04 04:36:45 +0800

    Add files via upload

---
+++ P1.c
@@ -0,0 +1,97 @@
+/* Filename:
+ *      client.c
+ *
+ * Descrition:
+ *      This program is used in tandem with server.c
+ *      This demonstrates the use of sockets to create a TCP client.
+ *
+ * Compile Instructions:
+ *      `gcc -o client client.c' or use the accompanying Makefile 
+ * 
+ * Author:
+ *      Glenn G. Fabia
+ *      glennfabia@adnu.edu.ph
+ *      Ateneo de Naga University
+ *
+ * Notes:
+ *      This is a handout for ICST240 - Operating Systems
+ *      Second Sem S/Y 2016-2017
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+
+void die_with_error(char *error_msg){
+    printf("%s", error_msg);
+    exit(-1);
+}
+
+int main(int argc,  char *argv[]){
+    
+    int client_sock,  port_no,  n;
+    struct sockaddr_in server_addr;
+    struct hostent *server;
+
+    char buffer[256];
+    if (argc < 3) {
+        printf("Usage: %s hostname port_no",  argv[0]);
+        exit(1);
+    }
+
+    printf("Client starting ...\n");
+    // Create a socket using TCP
+    client_sock = socket(AF_INET,  SOCK_STREAM,  0);
+    if (client_sock < 0) 
+        die_with_error("Error: socket() Failed.");
+
+    printf("Looking for host '%s'...\n", argv[1]);
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        die_with_error("Error: No such host.");
+    }
+    printf("Host found!\n");
+
+    // Establish a connection to server
+    port_no = atoi(argv[2]);
+    bzero((char *) &server_addr,  sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,  
+         (char *)&server_addr.sin_addr.s_addr, 
+         server->h_length);
+         
+    server_addr.sin_port = htons(port_no);
+
+    printf("Connecting to server at port %d...\n", port_no);
+    if (connect(client_sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) 
+        die_with_error("Error: connect() Failed.");
+
+    printf("Connection successful!\n");
+
+    // Communicate
+    printf("Please type a message : ");
+    bzero(buffer, 256);
+    fgets(buffer, 255, stdin);
+
+    printf("Sending message to server ...\n");
+    
+    n = send(client_sock, buffer, strlen(buffer), 0);
+    if (n < 0) 
+         die_with_error("Error: send() Failed.");
+         
+    printf("Message sent! Awaiting reply ...\n");
+    bzero(buffer, 256);
+    n = recv(client_sock, buffer, 255, 0);
+    if (n < 0) 
+         die_with_error("Error: recv() Failed.");
+    printf("Server says : %s\n", buffer);
+
+    close(client_sock);
+    
+    return 0;
+}
---
+++ P2.c
@@ -0,0 +1,91 @@
+/* Filename:
+ *      server.c
+ *
+ * Descrition:
+ *      This program is used in tandem with client.c
+ *      This demonstrates the use of sockets to create a TCP server.
+ *
+ * Compile Instructions:
+ *      `gcc -o server server.c' or use the accompanying Makefile 
+ * 
+ * Author:
+ *      Glenn G. Fabia
+ *      glennfabia@adnu.edu.ph
+ *      Ateneo de Naga University
+ *
+ * Notes:
+ *      This is a handout for ICST240 - Operating Systems
+ *      Second Sem S/Y 2016-2017
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <unistd.h>
+
+void die_with_error(char *error_msg){
+    printf("%s", error_msg);
+    exit(-1);
+}
+
+int main(int argc, char *argv[]){
+    int server_sock, client_sock, port_no, client_size, n;
+    char buffer[256];
+    struct sockaddr_in server_addr, client_addr;
+    if (argc < 2) {
+        printf("Usage: %s port_no", argv[0]);
+        exit(1);
+    }
+
+    printf("Server starting ...\n");
+    // Create a socket for incoming connections
+    server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock < 0) 
+       die_with_error("Error: socket() Failed.");
+       
+    // Bind socket to a port
+    bzero((char *) &server_addr, sizeof(server_addr));
+    port_no = atoi(argv[1]);
+    server_addr.sin_family = AF_INET; // Internet address 
+    server_addr.sin_addr.s_addr = INADDR_ANY; // Any incoming interface
+    server_addr.sin_port = htons(port_no); // Local port
+    
+    if (bind(server_sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) 
+       die_with_error("Error: bind() Failed.");
+       
+    // Mark the socket so it will listen for incoming connections
+    listen(server_sock, 5);
+    printf("Server listening to port %d ...\n", port_no);
+    
+    printf("Waiting for connection(s) ...\n");
+
+    // Accept new connection
+    client_size = sizeof(client_addr);
+    client_sock = accept(server_sock, (struct sockaddr *) &client_addr, &client_size);
+    if (client_sock < 0) 
+        die_with_error("Error: accept() Failed.");
+
+    printf("Client succesfully connected ...\n");    
+    // Communicate    
+    bzero(buffer, 256);
+    n = recv(client_sock, buffer, 255, 0);
+    if (n < 0) die_with_error("Error: recv() Failed.");
+    printf("Message received from client : %s", buffer);
+
+    printf("Sending reply ...\n");
+    bzero(buffer, 256);
+    fgets(buffer, 255, stdin);
+    n = send(client_sock, buffer, strlen(buffer), 0);
+    if (n < 0) die_with_error("Error: send() Failed.");
+
+    printf("Closing connection ...\n");
+    close(client_sock);
+    
+    close(server_sock);
+    
+    return 0; 
+}
